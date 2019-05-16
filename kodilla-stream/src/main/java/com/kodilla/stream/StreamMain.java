2010f8282f97bf479dd15ac99a3aa5a7f663db9f
ajanicka <janickanna@gmail.com>
2019-05-16 21:52:52 +0200

    Lambdas

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -0,0 +1,19 @@
+package com.kodilla.stream;
+
+import com.kodilla.stream.com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.lambda.Processor;
+
+public class StreamMain {
+    public static void main(String[] args) {
+        Processor processor = new Processor();
+        processor.execute(() -> System.out.println("This is an example text."));
+
+        PoemBeautifier beautifier = new PoemBeautifier();
+
+        System.out.println(beautifier.beautify("DEF", text -> "ABC"+text));
+        System.out.println(beautifier.beautify("XYZ", text -> text+"ABC"));
+        System.out.println(beautifier.beautify("Ala ma kota", text -> text.toUpperCase()));
+        System.out.println(beautifier.beautify("MALA ALA MA KOTA", text -> text.toLowerCase()));
+        System.out.println(beautifier.beautify("Last one", text -> "***"+text+"***"));
+    }
+}
