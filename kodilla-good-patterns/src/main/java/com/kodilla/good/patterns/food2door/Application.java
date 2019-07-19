89dc6e62c0d004847b80757e83e33d06255f7ca8
ajanicka <janickanna@gmail.com>
2019-07-19 21:20:33 +0200

    changes after review, delete not used variables

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/food2door/Application.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/food2door/Application.java
@@ -4,17 +4,13 @@ class Application {
 
     public static void main(String[] args) {
 
-        String provider;
-        String kindOfProduct;
-        int amount;
-
         OrderRetriever retriever = new OrderRetriever();
         Order order = retriever.retrieve();
 
         OrderService orderService = new OrderService();
 
-        boolean orderDone_1 = orderService.newOrder(new ExtraFoodShop(), order);
-        boolean orderDone_2 = orderService.newOrder(new GlutenFreeShop(), order);
-        boolean orderDone_3 = orderService.newOrder(new HealthyShop(), order);
+        orderService.newOrder(new ExtraFoodShop(), order);
+        orderService.newOrder(new GlutenFreeShop(), order);
+        orderService.newOrder(new HealthyShop(), order);
     }
 }
--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/food2door/OrderService.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/food2door/OrderService.java
@@ -2,7 +2,8 @@ package com.kodilla.good.patterns.food2door;
 
 class OrderService {
 
-    boolean newOrder(Producer producer, Order order) {
+    boolean newOrder(Producer producer, Order order)
+    {
         return producer.process(order);
     }
 }
