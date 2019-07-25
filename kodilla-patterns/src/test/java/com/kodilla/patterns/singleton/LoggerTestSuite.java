7e47ccf44ae790563bc70d368a5bbfb918ca7dd5
ajanicka <janickanna@gmail.com>
2019-07-25 16:46:19 +0200

    changes after review

--- kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
@@ -1,29 +1,32 @@
 package com.kodilla.patterns.singleton;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class LoggerTestSuite {
 
+    private Logger logger;
+
+    @Before
+    public void createLoggerWithOneLog() {
+        logger = Logger.getInstance();
+        logger.log("log1");
+    }
+
     @Test
     public void verifyAddedLog() {
-        //Given
-        Logger logger = Logger.getInstance();
-
-        //When
-        logger.log("log1");
-
         //Then
         assertEquals("log1", logger.getLastLog());
     }
 
     @Test
-    public void getNoLogs() {
+    public void verifyNoSecondLogger() {
         //Given
-        Logger logger = Logger.getInstance();
+        Logger logger2 = Logger.getInstance();
 
         //Then
-        assertEquals("log1", logger.getLastLog());
+        assertEquals("log1", logger2.getLastLog());
     }
 }
