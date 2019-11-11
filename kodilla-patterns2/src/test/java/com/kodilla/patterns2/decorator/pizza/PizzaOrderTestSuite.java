b9f1bec62821f39ab5b0c1293c9900e34581682b
ajanicka <janickanna@gmail.com>
2019-11-11 20:30:20 +0100

    24_2

---
+++ kodilla-patterns2/src/test/java/com/kodilla/patterns2/decorator/pizza/PizzaOrderTestSuite.java
@@ -0,0 +1,80 @@
+package com.kodilla.patterns2.decorator.pizza;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.math.BigDecimal;
+
+import static org.junit.Assert.assertEquals;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class PizzaOrderTestSuite {
+    @Test
+    public void testBasicTaxiOrderGetCost() {
+        // Given
+        PizzaOrder theOrder = new BasicPizzaOrder();
+        // When
+        BigDecimal calculatedCost = theOrder.getCost();
+        // Then
+        assertEquals(new BigDecimal(15), calculatedCost);
+    }
+
+    @Test
+    public void testBasicTaxiOrderGetDescription() {
+        // Given
+        PizzaOrder theOrder = new BasicPizzaOrder();
+        // When
+        String description = theOrder.getDescription();
+        // Then
+        assertEquals("Basic pizza with tomato sauce and cheese", description);
+    }
+
+    @Test
+    public void testHawaiiPizzaOrderGetCost() {
+        // Given
+        PizzaOrder theOrder = new BasicPizzaOrder();
+        theOrder = new PineaplePizzaDecorator(theOrder);
+        // When
+        BigDecimal calculatedCost = theOrder.getCost();
+        // Then
+        assertEquals(new BigDecimal(20), calculatedCost);
+    }
+
+    @Test
+    public void testHawaiiPizzaOrderGetDescription() {
+        // Given
+        PizzaOrder theOrder = new BasicPizzaOrder();
+        theOrder = new PineaplePizzaDecorator(theOrder);
+        // When
+        String description = theOrder.getDescription();
+        // Then
+        assertEquals("Basic pizza with tomato sauce and cheese + pineaple", description);
+    }
+
+    @Test
+    public void testHawaiiPizzaWithAdditionalCheeseOrderGetCost() {
+        // Given
+        PizzaOrder theOrder = new BasicPizzaOrder();
+        theOrder = new PineaplePizzaDecorator(theOrder);
+        theOrder = new AdditionalCheeseDecorator(theOrder);
+        // When
+        BigDecimal calculatedCost = theOrder.getCost();
+        // Then
+        assertEquals(new BigDecimal(23), calculatedCost);
+    }
+
+    @Test
+    public void testHawaiiPizzaWithAdditionalCheeseOrderGetDescription() {
+        // Given
+        PizzaOrder theOrder = new BasicPizzaOrder();
+        theOrder = new PineaplePizzaDecorator(theOrder);
+        theOrder = new AdditionalCheeseDecorator(theOrder);
+        // When
+        String description = theOrder.getDescription();
+        // Then
+        assertEquals("Basic pizza with tomato sauce and cheese + pineaple + additional cheese", description);
+    }
+}
